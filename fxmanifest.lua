4b8a9a50bff017aa488f6051246137c2a3c078f9
JarretOnline <90429932+JarretOnline@users.noreply.github.com>
2022-06-10 12:35:03 -0600

    Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5' 
+
+author 'sjpfeiffer'
+description 'A ped spawner for fivem'
+version '1.2.0'
+
+lua54 'yes'
+
+dependencies {
+	"qtarget"
+}
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
